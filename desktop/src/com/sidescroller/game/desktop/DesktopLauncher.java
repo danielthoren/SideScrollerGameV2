19b2157ac9f862c9a5b8e2c0a17c5e5d83e64b2a
daniel <danne_thoren456@hotmail.com>
2016-06-05 18:00:26 +0200

    First commit after creating a libGDX project using the executable.

---
+++ desktop/src/com/sidescroller/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sidescroller.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sidescroller.game.SideScrollerGameV2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SideScrollerGameV2(), config);
+	}
+}
