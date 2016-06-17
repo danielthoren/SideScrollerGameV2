ac6c40eb2a43b5b10b69c6004821f937d4b0dc49
Alexander Wilkens <alewi684@student.liu.se>
2016-06-17 16:29:49 +0200

    sdfsd

--- core/src/com/sidescroller/player/Player.java
+++ core/src/com/sidescroller/player/Player.java
@@ -174,8 +174,8 @@ public class Player implements Draw, Update, InputListener, CollisionListener {
      * The function that updates the object every frame
      */
     public void update() {
+        System.out.println("hello");
         runHandler();
-
         //Ensures that the sensorvalue is not wrong. If velocity.y is 0 for two frames then the character is isGrounded
         if (body.getLinearVelocity().y > -GROUNDED_THRESHOLD && body.getLinearVelocity().y < GROUNDED_THRESHOLD && !isGrounded){
             if (groundResetTimer == -1){groundResetTimer = System.currentTimeMillis();}
